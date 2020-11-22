42d3f9fe06e2820a2317f746d21ebabdc71c666a
Vorobiov Vlad <vorobyov.vladislav@gmail.com>
2020-11-22 14:56:18 +0200

    Add sorry

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 function askNameAndSayHello() {
-  const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('Sorry? May I have your name? ');
   console.log(`Hello, ${name}!`);
 }
 
